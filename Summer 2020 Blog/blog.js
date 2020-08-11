e9943ba6e94b7be34328039510b1f63d6462610b
Etienne Richart <etiennerichart@gmail.com>
2020-08-11 01:23:01 -0400

    add blog homepage

---
+++ Summer 2020 Blog/blog.js
@@ -0,0 +1,10 @@
+function showInfo(section) {
+    var info = document.getElementById(section);
+    info.style.display = "block";
+}
+
+function hideInfo(section) {
+    var info = document.getElementById(section);
+    info.style.display = "none";
+}
+
